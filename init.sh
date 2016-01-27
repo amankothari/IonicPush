12d1457543fe56cb1e21c554c6eafa011a32be21
aman kothari <amankothari85@gmail.com>
2016-01-27 12:51:54 +0530

    First Commit

---
+++ init.sh
@@ -0,0 +1,10 @@
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add com.ionic.keyboard
+ionic plugin add https://github.com/phonegap-build/PushPlugin.git
+ionic add ngCordova
+ionic add ionic-service-core
+ionic add ionic-service-push
+ionic add angular-websocket
+npm install && bower install
